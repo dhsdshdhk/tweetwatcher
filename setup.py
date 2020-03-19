4249ae1afe8e393a996168821a5297bcf720dc60
dhsdshdhk <28062763+dhsdshdhk@users.noreply.github.com>
2020-03-18 21:15:53 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tweetwatcher", # Replace with your own username
-    version="0.0.2",
+    name="tweetwatcher",
+    version="0.0.3",
     author="John Doe",
     author_email="author@example.com",
     description="A simple tweet watcher.",
